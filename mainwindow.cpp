2e697d255a2e95b42d617cd61245d88a226f1cc0
bdrackley <brendandrackley@gmail.com>
2017-07-26 15:20:51 -0500

    formatted to single-table database, added export results feature, added contains vs. matches, added location and mutation type filters, added stylistic changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,9 @@
 #include <QSqlTableModel>
 #include <QTableView>
 #include <QSqlQuery>
+#include <QFile>
+#include <QMessageBox>
+#include <QFileDialog>
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -12,25 +15,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowFlags(Qt::Window | Qt::MSWindowsFixedSizeDialogHint);
+    this->statusBar()->setSizeGripEnabled(false);
+
     fieldList = "Gene,Chromosome,Sequence,SNP,miRNA,AlleleNum,Alleles,\"MAF_>0.05\",Location,Mutation_ProteinPos,Mutation_Type";
     comboBoxSelection = "Gene";
-    geneOn = true;
-    miRNAON = false;
-    snpON = false;
+    optionChar = "%";
+    mutType = "";
+    snpLocation = "";
 
     miRNA_SNP_Database = QSqlDatabase::addDatabase("QSQLITE");
-    miRNA_SNP_Database.setDatabaseName("miRNA_SNP.sqlite");
+    miRNA_SNP_Database.setDatabaseName("miRNA_SNP_Data.sqlite");
     miRNA_SNP_Database.open();
     tables = miRNA_SNP_Database.tables(QSql::Tables);
 
     model = new QSqlQueryModel;
-    query = "";
-    for (int i = 0; i < tables.size(); i++){
-        query += "SELECT " + fieldList + " FROM " + tables.at(i) + " ";
-        query += "UNION ALL ";
-    }
-    query.remove(query.size()-11,11);
-    //    qDebug() << query;
+    query = "SELECT " + fieldList + " FROM miRNA_SNP_Data";
     model->setQuery(query);
 
     for(int i=0;i<4;i++)
@@ -41,6 +41,10 @@ MainWindow::MainWindow(QWidget *parent) :
     model->setHeaderData(9, Qt::Horizontal, tr("Residue Number"));
     model->setHeaderData(10, Qt::Horizontal, tr("Mutation Type"));
     ui->mainTable->setModel(model);
+    QRect rect = ui->mainTable->geometry();
+    colWidth = ui->mainTable->columnWidth(0);
+    rect.setWidth(20 + ui->mainTable->verticalHeader()->width() + colWidth * 11);
+    ui->mainTable->setGeometry(rect);
     rowResults = model->rowCount();
     if(rowResults<=1000)
         ui->resultsOutput->setText(QString::number(rowResults));
@@ -55,19 +59,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_searchButton_clicked()
 {
-    query = "";
-    for (int i = 0; i < tables.size(); i++){
-        query += "SELECT " + fieldList + " FROM " + tables.at(i) + " ";
-        if(geneOn){
-            query += "WHERE Gene LIKE '%" + searchParam + "%' ";
-        }else if(miRNAON){
-            query += "WHERE miRNA LIKE '%" + searchParam + "%' ";
-        }else if(snpON){
-            query += "WHERE SNP LIKE '%" + searchParam + "%' ";
-        }
-        query += "UNION ALL ";
-    }
-    query.remove(query.size()-11,11);
+    query = "SELECT " + fieldList + " FROM miRNA_SNP_Data ";
+    query += "WHERE " + comboBoxSelection + " LIKE '" + optionChar + searchParam + optionChar + "' ";
+    query += "AND Location LIKE '%" + snpLocation + "%' ";
+    query += "AND Mutation_Type LIKE '%" + mutType + "%' ";
     //    qDebug() << query;
     model->setQuery(query);
 
@@ -90,39 +85,94 @@ void MainWindow::on_searchBar_textChanged(const QString &arg1)
 void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
     comboBoxSelection = arg1;
-    if(comboBoxSelection.compare("Gene")==0){
-        geneOn = true;
-        miRNAON = false;
-        snpON = false;
-    }else if(comboBoxSelection.compare("miRNA")==0){
-        geneOn = false;
-        miRNAON = true;
-        snpON = false;
-    }else if(comboBoxSelection.compare("SNP")==0){
-        geneOn = false;
-        miRNAON = false;
-        snpON = true;
-    }
 }
 
 void MainWindow::on_resetButton_clicked()
 {
-    query = "";
-    for (int i = 0; i < tables.size(); i++){
-        query += "SELECT " + fieldList + " FROM " + tables.at(i) + " ";
-        query += "UNION ALL ";
-    }
-    query.remove(query.size()-11,11);
+    query = "SELECT " + fieldList + " FROM miRNA_SNP_Data";
     model->setQuery(query);
     ui->searchBar->setText("");
+    ui->comboBox->setCurrentIndex(0);
+    ui->comboBox_2->setCurrentIndex(0);
+    ui->locationComboBox->setCurrentIndex(0);
+    ui->mutTypeComboBox->setCurrentIndex(0);
 
     for(int i=0;i<4;i++)
         model->fetchMore();
 
     ui->mainTable->setModel(model);
+    for(int i = 0; i < model->columnCount(); i++){
+        ui->mainTable->setColumnWidth(i,colWidth);
+    }
+
     rowResults = model->rowCount();
     if(rowResults<=1000)
         ui->resultsOutput->setText(QString::number(rowResults));
     else
         ui->resultsOutput->setText(">1000");
+}
+
+void MainWindow::on_comboBox_2_currentIndexChanged(const QString &arg1)
+{
+    if(arg1.compare("Contains")==0){
+        optionChar = "%";
+    }else if(arg1.compare("Matches")==0){
+        optionChar = "";
+    }
+}
+
+void MainWindow::on_locationComboBox_currentIndexChanged(const QString &arg1)
+{
+    if(arg1.compare("All")==0){
+        snpLocation = "";
+    }else if(arg1.compare("5UTR")==0){
+        snpLocation = "5UTR";
+    }else if(arg1.compare("Gene")==0){
+        snpLocation = "gene";
+    }else if(arg1.compare("3UTR")==0){
+        snpLocation = "3UTR";
+    }
+}
+
+void MainWindow::on_mutTypeComboBox_currentIndexChanged(const QString &arg1)
+{
+    if(arg1.compare("All")==0){
+        mutType = "";
+    }else if(arg1.compare("Syn")==0){
+        mutType = "Syn";
+    }else if(arg1.compare("Mis")==0){
+        mutType = "Mis";
+    }
+}
+
+void MainWindow::on_exportResultsButton_clicked()
+{
+    if(rowResults <= 1000){
+        QString filename = QFileDialog::getSaveFileName(this, tr("Export File"), QString(), tr("CSV Files (*.csv)"));
+        QFile file(filename);
+        if(file.open(QIODevice::WriteOnly | QIODevice::Truncate)){
+            QTextStream output(&file);
+            output << fieldList << "\r\n";
+            file.close();
+        }
+        if(file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+            QString textData;
+            int columns = model->columnCount();
+            for (int i = 0; i < rowResults; i++) {
+                textData = "";
+                for (int j = 0; j < columns; j++) {
+                    textData += model->data(model->index(i,j)).toString();
+                    textData += ", ";
+                }
+                textData += "\r\n";
+                QTextStream output(&file);
+                output << textData;
+            }
+            file.close();
+            QMessageBox::information(this, "Success", "Search results have been exported");
+        }
+    }else{
+        //system dialog: too much data to export
+        QMessageBox::critical(this, tr("Error"), "Cannot export >1000 results");
+    }
 }
